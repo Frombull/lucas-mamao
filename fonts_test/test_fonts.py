9ad6d5dc19c41ea3eb627f014a214ae4d916a9b3
Frombull <marcorrditoro@gmail.com>
2022-02-21 13:58:21 -0300

    Organize fonts test

--- fonts_test/test_fonts.py
+++ fonts_test/test_fonts.py
@@ -1,25 +1,26 @@
 from PIL import Image, ImageDraw, ImageFont
 from os import listdir, remove
 from uuid import uuid4
+import logging as log
 
 TEST_IMAGE = 'mamao.jpg'
-FONTS_FOLDER = 'test_fonts'
-IMAGES_FOLDER_IN = 'tmp/test_images_in'
-IMAGES_FOLDER_OUT = 'tmp/test_images_out'
+TEST_FONTS_FOLDER = 'test_fonts'
+IMAGES_FOLDER_IN = 'media/test_images_in'
+IMAGES_FOLDER_OUT = 'media/test_images_out'
 
 
 def write_on_image(image_path: str, font: str):
     print('-' * 40)
-    print(f'Writing on: {image_path}')
+    log.info(f'Writing on: {image_path}')
 
     image_name = image_path.split('/')[-1]
     image_pil = Image.open(image_path)
     image_w, image_h = image_pil.size
 
     font_size = int(image_w / 8)
-    font_path = f'{FONTS_FOLDER}/{font}'
+    font_path = f'{TEST_FONTS_FOLDER}/{font}'
     font_pil = ImageFont.truetype(font_path, size=font_size)
-    print(f'Using font: {font_path.split("fonts/")[1]}')
+    log.info(f'Using font: {font_path.split("fonts/")[1]}')
 
     image_pil_edit = ImageDraw.Draw(image_pil)
     image_pil_edit.text(
@@ -45,28 +46,28 @@ def write_on_image(image_path: str, font: str):
     random_str = str(uuid4()).split('-')[0]
     img_type = image_name.split('.')[1]
     final_name = f'{IMAGES_FOLDER_OUT}/{font_name_only + "-" + random_str}.{img_type}'
-    # final name e.g. tmp/test_images_out/arial-07b39f44.jpg
+    # final name e.g. media/test_images_out/arial-07b39f44.jpg
 
     image_pil.save(final_name)
 
 
 def delete_old_images(folder_path: str):
     print('-' * 80)
-    print(f'Removing old images from {folder_path}')
+    log.info(f'Removing old images from {folder_path}')
 
     for file in listdir(path=folder_path):
         if file.endswith('.jpg'):
             print(f'Removing {file}')
             remove(f'{folder_path}/{file}')
 
-    print('Done removing old images')
+    log.info('Done removing old images')
     print('-' * 80)
 
 
 def main():
     delete_old_images(folder_path=IMAGES_FOLDER_OUT)
 
-    for font in listdir(FONTS_FOLDER):
+    for font in listdir(TEST_FONTS_FOLDER):
         write_on_image(image_path=f'{IMAGES_FOLDER_IN}/{TEST_IMAGE}',
                        font=font)
 
